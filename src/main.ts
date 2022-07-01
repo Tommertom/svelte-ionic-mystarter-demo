a771840689bd12e05442758d14102bb0c6101d7a
Tommertom <Tom.Gruintjes@gmail.com>
2022-07-01 17:35:30 +0200

    CDN works

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,14 @@ document.querySelector('[data-routify]')?.remove()
 const app = new App({
   target: document.getElementById('app')
 })
-defineCustomElements(window);
+// defineCustomElements(window);
 export default app
 
 /*
-
+  <script src="https://cdn.jsdelivr.net/npm/exif-js"></script>
   <script type="module"
-    src="https://unpkg.com/@ionic/pwa-elements@latest/dist/ionicpwaelements/ionicpwaelements.esm.jspwa-elements@latest/dist/ionicpwaelements/ionicpwaelements.esm.js"></script>
+    src="https://unpkg.com/@ionic/pwa-elements@latest/dist/ionicpwaelements/ionicpwaelements.esm.js"></script>
   <script nomodule
-    src="https://unpkg.com/@ionic/pwa-elements@latest/dist/ionicpwaelements/ionicpwaelements.jspwa-elements@latest/dist/ionicpwaelements/ionicpwaelements.js"></script>
-
+    src="https://unpkg.com/@ionic/pwa-elements@latest/dist/ionicpwaelements/ionicpwaelements.js"></script>
 
 */
